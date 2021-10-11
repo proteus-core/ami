579040ad3c8cd43d1ccc47520e39b8b917200c29
Hans Winderix <hans.winderix@skynet.be>
2021-10-11 14:10:53 +0200

    Add pipeline registers

--- src/main/scala/riscv/plugins/Mimicry.scala
+++ src/main/scala/riscv/plugins/Mimicry.scala
@@ -11,31 +11,50 @@ class Mimicry(decodeStage: Stage) extends Plugin[Pipeline] {
   }
 
   object Data {
-    object MIMICRY_IS_ENABLED extends PipelineData(Bool())
+    object ENABLE_MIMICRY_ONCE  extends PipelineData(Bool()) // Mimic behavior for the current instruction only
+    object ENABLE_MIMICRY_MODE  extends PipelineData(Bool()) // Enable mimicry mode
+    object DISABLE_MIMICRY_MODE extends PipelineData(Bool()) // Disable mimicry mode
+    object IGNORE_MIMICRY_MODE  extends PipelineData(Bool()) // Ignore mimicry mode for the current instruction only
+
+    object MIMIC                extends PipelineData(Bool()) // Propagates to further pipeline stages if the
+                                                             // the current instruction must be mimicked
   }
 
   override def setup(): Unit = {
-    decodeStage plug new Area {
-      private val inMimicryMode = Reg(Bool).init(False)
-    }
-
     pipeline.getService[DecoderService].configure { config =>
       config.addDefault(Map(
-        Data.MIMICRY_IS_ENABLED -> False
+        Data.ENABLE_MIMICRY_ONCE  -> False,
+        Data.ENABLE_MIMICRY_MODE  -> False,
+        Data.DISABLE_MIMICRY_MODE -> False,
+        Data.IGNORE_MIMICRY_MODE  -> False,
+        Data.MIMIC                -> False
       ))
 
       config.addDecoding(Opcodes.EMM, InstructionType.I, Map(
-        Data.MIMICRY_IS_ENABLED -> True
+        Data.ENABLE_MIMICRY_MODE -> True
       ))
 
       config.addDecoding(Opcodes.DMM, InstructionType.I, Map(
-        Data.MIMICRY_IS_ENABLED -> False
+        Data.DISABLE_MIMICRY_MODE -> False
       ))
     }
   }
 
   override def build(): Unit = {
     decodeStage plug new Area {
+      import decodeStage._
+
+      private val inMimicryMode = Reg(Bool).init(False)
+
+      when (value(Data.ENABLE_MIMICRY_MODE)) {
+        inMimicryMode  := True
+      }
+
+      when (value(Data.DISABLE_MIMICRY_MODE)) {
+        inMimicryMode  := False
+      }
+
+      output(Data.MIMIC) := inMimicryMode
     }
   }
 }
