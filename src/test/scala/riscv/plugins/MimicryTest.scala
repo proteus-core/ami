d652355210b57f43006aac7aecdc1a852275774c
Hans Winderix <hans.winderix@skynet.be>
2021-10-11 15:07:46 +0200

    Setup microarchitectural tests, work in progress

--- src/test/scala/riscv/plugins/MimicryTest.scala
+++ src/test/scala/riscv/plugins/MimicryTest.scala
@@ -1,9 +1,17 @@
 package riscv.plugins
 
 import org.scalatest.funsuite.AnyFunSuite
+import riscv.SoC
+import riscv.soc.RamType
+import spinal.core.IntToBuilder
+import spinal.core.sim.{SimClockDomainHandlePimper, SimConfig}
 
 class MimicryTest extends AnyFunSuite {
   test("Mimicry") {
-    assert(true)
+    val hexFile = "src/test/resources/hex/mimicry.ihex"
+    SimConfig.withWave.compile(SoC.static(RamType.OnChipRam(10 MiB, Some(hexFile)))).doSim { dut =>
+      dut.clockDomain.forkStimulus(10)
+      dut.clockDomain.waitSampling()
+    }
   }
 }
