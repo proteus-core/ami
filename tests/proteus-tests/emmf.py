7e010671640bd4fe30e4454426d3b9f5aa56e2c6
Hans Winderix <hans.winderix@skynet.be>
2021-10-20 09:24:47 +0200

    Add emmf regression test

---
+++ tests/proteus-tests/emmf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import ProteusTest
+
+class emmf(ProteusTest.ProteusTest):
+
+  def on_change_writeback_pc(self, vcd, m_addr, t, pc):
+
+    inMM = self.as_int(vcd, self.WB.Mimicry_inMimicryMode, self.nextt(t))
+
+    if pc == m_addr:
+      self.assertEqual(inMM, 1)
+
+if __name__ == '__main__':
+  emmf(len(sys.argv) > 1)
