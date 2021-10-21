b4bb1e1f7260e6d6d8cc3663b3c576f4225818d2
Hans Winderix <hans.winderix@cs.kuleuven.be>
2021-10-21 16:32:59 +0200

    Test pipeline propagation of MIMiC RD_TYPE

--- tests/proteus-tests/emm.py
+++ tests/proteus-tests/emm.py
@@ -8,7 +8,26 @@ class emm(MimicryTest.MimicryTest):
   def on_change_decode_pc(self, vcd, m_addr, t, pc):
 
     if pc == m_addr:
-      self.assertTrue(self.inMM(vcd, self.nextt(t)))
+      tn = self.nextt(t)
+      self.assertTrue(self.inMM(vcd, tn))
+      self.assertEqual(self.as_int(vcd, self.PL.decode_out_RD_TYPE, tn), 2)
+
+    if pc == m_addr+4:
+      self.assertTrue(self.inMM(vcd, t))
+      self.assertEqual(self.as_int(vcd, self.PL.decode_out_RD_TYPE, t), 2)
+
+  def on_change_execute_pc(self, vcd, m_addr, t, pc):
+    #TODO: if pc in (m_addr, m_addr+4):
+    if pc == m_addr+4:
+      self.assertEqual(self.as_int(vcd, self.PL.execute_out_RD_TYPE, t), 2)
+
+  def on_change_memory_pc(self, vcd, m_addr, t, pc):
+    if pc == m_addr+4:
+      self.assertEqual(self.as_int(vcd, self.PL.memory_out_RD_TYPE, t), 2)
+
+  def on_change_writeback_pc(self, vcd, m_addr, t, pc):
+    if pc == m_addr+4:
+      self.assertEqual(self.as_int(vcd, self.PL.writeback_out_RD_TYPE, t), 2)
 
 if __name__ == '__main__':
   emm(len(sys.argv) > 1)
