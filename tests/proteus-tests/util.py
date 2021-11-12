e260214b863b88471c3a5b575b0258f913671144
Hans Winderix <hans.winderix@skynet.be>
2021-11-12 13:43:48 +0100

    Use llvm-objdump to disassemble

--- tests/proteus-tests/util.py
+++ tests/proteus-tests/util.py
@@ -3,6 +3,48 @@ import subprocess
 import tempfile
 
 ###########################################################################
+def gnu_objdump(f):
+  objdump  = "riscv64-unknown-elf-objdump"
+  objdump += " -D"
+  objdump += " -b binary"
+  objdump += " -m riscv"
+  objdump += " -M no-aliases"
+  objdump += " %s" % f.name
+
+  return subprocess.run(objdump.split(),
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        universal_newlines=True)
+
+###########################################################################
+def gnu_objcopy(f1, f2):
+  result = tempfile.NamedTemporaryFile()
+
+  objcopy  = "riscv32-unknown-elf-objcopy"
+  objcopy += " -I binary"
+  objcopy += " -O elf32-littleriscv"
+  objcopy += " %s" % f1.name
+  objcopy += " %s" % f2.name
+
+  subprocess.run(objcopy.split(),
+                 stdout=subprocess.DEVNULL,
+                 stderr=subprocess.DEVNULL)
+
+###########################################################################
+def llvm_objdump(f):
+  with tempfile.NamedTemporaryFile() as f2:
+    gnu_objcopy(f, f2)
+
+    objdump  = "/usr/local/morpheus/bin/llvm-objdump"
+    objdump += " -D"
+    objdump += " %s" % f2.name
+  
+    return subprocess.run(objdump.split(),
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          universal_newlines=True)
+  
+###########################################################################
 def disassemble(bytez):
 
   assert len(bytez) == 4
@@ -11,25 +53,16 @@ def disassemble(bytez):
     f.write(bytez)
     f.flush()
 
-    objdump  = "riscv64-unknown-elf-objdump"
-    objdump += " -D"
-    objdump += " -b binary"
-    objdump += " -m riscv"
-    objdump += " -M no-aliases"
-    objdump += " %s" % f.name
-
-    proc = subprocess.run(objdump.split(),
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE,
-                          universal_newlines=True)
+    proc = llvm_objdump(f)
 
     if proc.returncode == 0:
       for line in proc.stdout.split('\n'):
         parts = line.split()
         if len(parts) >= 3 and parts[0] == '0:':
-          return ' '.join(parts[2:])
+          #return ' '.join(parts[2:])
+          return ' '.join(parts[5:])
 
-    return bytez.tostring()
+    return bytez.hex()
  
 #############################################################################
 class NestedNamespace(SimpleNamespace):
