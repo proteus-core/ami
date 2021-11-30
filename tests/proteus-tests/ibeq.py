69086f921d31df0d67f5b184ced62b0333b49a2f
Hans Winderix <hans.winderix@scarlet.be>
2021-11-30 14:30:40 +0100

    Test i.beq instruction

--- tests/proteus-tests/ibeq.py
+++ tests/proteus-tests/ibeq.py
@@ -7,10 +7,31 @@ class ibeq(MimicryTest.MimicryTest):
 
   def on_change_writeback_pc(self, vcd, t, pc):
 
+    mark0 = vcd.get_marked_instr_addr(0x00)
+    mark1 = vcd.get_marked_instr_addr(0x01)
+
+    is_invert = vcd.as_int(vcd.WB.value_INVERT, t) == 1
+    is_taken = vcd.as_int(vcd.WB.value_OUTCOME, t) == 1
     depth = vcd.as_int(vcd.CSR.CsrFile_depth, t)
 
-    if pc == vcd.get_marked_instr_addr():
-      pass
+    if pc == mark0:
+      self.assertTrue(is_invert)
+      self.assertTrue(is_taken)
+      self.assertFalse(self.in_mm(vcd, t))
+      self.assertEqual(depth, 0)
+
+    if pc == mark0 + 4:
+      self.assertTrue(self.in_mm(vcd, t))
+      self.assertEqual(depth, 1)
+
+    if pc == mark1:
+      self.assertTrue(is_invert)
+      self.assertFalse(is_taken)
+      self.assertEqual(depth, 1)
+
+    if pc == mark1 + 4:
+      self.assertFalse(self.in_mm(vcd, t))
+      self.assertEqual(depth, 0)
 
 if __name__ == '__main__':
   ibeq(len(sys.argv) > 1)
