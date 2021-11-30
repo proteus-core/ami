44efb620457e97e8cf387da05082727ea7ccd82e
Hans Winderix <hans.winderix@scarlet.be>
2021-11-30 12:31:00 +0100

    Add m.jal(r) regression tests, work in progress

--- tests/proteus-tests/mj.py
+++ tests/proteus-tests/mj.py
@@ -4,7 +4,56 @@ import sys
 import MimicryTest
 
 class mj(MimicryTest.MimicryTest):
-  pass
+
+  def on_change_writeback_pc(self, vcd, t, pc):
+
+    tn = vcd.nextt(t)
+
+    mark1 = vcd.get_marked_instr_addr(0x01)
+    mark2 = vcd.get_marked_instr_addr(0x02)
+
+    is_mimic = vcd.as_int(vcd.WB.value_MIMIC, t) == 1
+    is_jump = vcd.as_int(vcd.WB.value_ISJUMP, t) == 1
+
+    depth  = vcd.as_int(vcd.CSR.CsrFile_depth, t)
+    depthn = vcd.as_int(vcd.CSR.CsrFile_depth, tn)
+
+    exit = vcd.as_int(vcd.CSR.CsrFile_exit, t)
+    exitn = vcd.as_int(vcd.CSR.CsrFile_exit, tn)
+
+    if pc == mark1:
+      self.assertTrue(is_mimic)
+      self.assertTrue(is_jump)
+      self.assertFalse(self.in_mm(vcd, t))
+      self.assertTrue(self.in_mm(vcd, tn))
+      self.assertEqual(depth, 0)
+      self.assertEqual(depthn, 1)
+      self.assertEqual(exitn, mark1+4)
+
+    if pc == mark2:
+      self.assertTrue(is_mimic)
+      self.assertTrue(is_jump)
+      self.assertFalse(self.in_mm(vcd, t))
+      self.assertTrue(self.in_mm(vcd, tn))
+      self.assertEqual(depth, 0)
+      self.assertEqual(depthn, 1)
+      self.assertEqual(exitn, mark2+4)
+
+    if pc == vcd.get_marked_instr_addr(0x11):
+      self.assertTrue(self.in_mm(vcd, t))
+      self.assertEqual(depth, 1)
+      self.assertEqual(exit, mark1+4)
+
+    if pc == vcd.get_marked_instr_addr(0x22):
+      self.assertTrue(self.in_mm(vcd, t))
+      self.assertEqual(depth, 1)
+      self.assertEqual(exit, mark2+4)
+      
+    if pc == vcd.get_marked_instr_addr(0x222):
+      self.assertTrue(self.in_mm(vcd, t))
+      self.assertEqual(depth, 1)
+      self.assertEqual(exit, mark2+4)
+
 
 if __name__ == '__main__':
   mj(len(sys.argv) > 1)
