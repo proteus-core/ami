a93c45bb2eb785ec931ab2e506d3bfc4e77ea546
Hans Winderix <hans.winderix@kuleuven.be>
2021-12-07 13:49:17 +0100

    Test recursive calls with growing activation counter

--- tests/proteus-tests/mj.py
+++ tests/proteus-tests/mj.py
@@ -69,5 +69,51 @@ class mj(MimicryTest.MimicryTest):
     self.assertEqual(self.mm_entry(vcd, t), mark2.addr)
     self.assertEqual(self.mm_exit(vcd, t), mark2.addr+4)
 
+    # Mark 33x - Tests recursive calls with depth > 1
+    mark330 = vcd.get_mark(0x330)
+    mark331 = vcd.get_mark(0x331)
+    mark332 = vcd.get_mark(0x332)
+    assert(len(mark330.WB) == 3)
+    assert(len(mark331.WB) == 2)
+    assert(len(mark332.WB) == 3)
+
+    # Mark 330 (start)
+    t = mark330.WB[0]
+    self.assertFalse(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 0)
+    self.assertEqual(self.mm_entry(vcd, t), 0x7FFFFFFF)
+    self.assertEqual(self.mm_exit(vcd, t), 0x7FFFFFFF)
+
+    t = mark330.WB[1]
+    self.assertTrue(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 1)
+    self.assertEqual(self.mm_entry(vcd, t), mark331.addr)
+    self.assertEqual(self.mm_exit(vcd, t), mark331.addr+4)
+
+    t = mark330.WB[2]
+    self.assertTrue(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 2)
+    self.assertEqual(self.mm_entry(vcd, t), mark331.addr)
+    self.assertEqual(self.mm_exit(vcd, t), mark331.addr+4)
+
+    # Mark 332 (return)
+    t = mark332.WB[0]
+    self.assertTrue(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 2)
+    self.assertEqual(self.mm_entry(vcd, t), mark331.addr)
+    self.assertEqual(self.mm_exit(vcd, t), mark331.addr+4)
+
+    t = mark332.WB[1]
+    self.assertTrue(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 1)
+    self.assertEqual(self.mm_entry(vcd, t), mark331.addr)
+    self.assertEqual(self.mm_exit(vcd, t), mark331.addr+4)
+
+    t = mark332.WB[2]
+    self.assertFalse(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 0)
+    self.assertEqual(self.mm_entry(vcd, t), 0x7FFFFFFF)
+    self.assertEqual(self.mm_exit(vcd, t), 0x7FFFFFFF)
+
 if __name__ == '__main__':
   mj(len(sys.argv) > 1)
