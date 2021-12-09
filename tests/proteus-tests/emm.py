885b0b36f20883bcb1be2032fbad1db76ce72934
Hans Winderix <hans.winderix@kuleuven.be>
2021-12-09 17:11:30 +0100

    Cleanup emm test

--- tests/proteus-tests/emm.py
+++ tests/proteus-tests/emm.py
@@ -5,28 +5,6 @@ import MimicryTest
 
 class emm(MimicryTest.MimicryTest):
 
-  """
-  def on_change_decode_pc(self, vcd, t, pc):
-
-    if pc == m_addr:
-      tn = vcd.nextt(t)
-      self.assertTrue(self.in_mm(vcd, tn))
-      self.assertEqual(vcd.as_int(vcd.PL.decode_out_RD_TYPE, tn), 2)
-
-    if pc == m_addr+4:
-      self.assertTrue(self.in_mm(vcd, t))
-      self.assertEqual(vcd.as_int(vcd.PL.decode_out_RD_TYPE, t), 2)
-
-  def on_change_execute_pc(self, vcd, m_addr, t, pc):
-    #TODO: if pc in (m_addr, m_addr+4):
-    if pc == m_addr+4:
-      self.assertEqual(vcd.as_int(vcd.PL.execute_out_RD_TYPE, t), 2)
-
-  def on_change_memory_pc(self, vcd, m_addr, t, pc):
-    if pc == m_addr+4:
-      self.assertEqual(vcd.as_int(vcd.PL.memory_out_RD_TYPE, t), 2)
-  """
-
   def run(self, vcd):
 
     mark = vcd.get_mark()
