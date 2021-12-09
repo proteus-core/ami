f3b8fd68c02926992d0e7dd16a1f16e73d660c0d
Hans Winderix <hans.winderix@kuleuven.be>
2021-12-09 17:25:40 +0100

    Add test for activating nested branch

--- tests/proteus-tests/ite.py
+++ tests/proteus-tests/ite.py
@@ -45,5 +45,29 @@ class ite(MimicryTest.MimicryTest):
     t = vcd.WB2[mark3.addr+12][0]
     self.assertEqual(vcd.as_int(vcd.WB.out_RD_TYPE, t), 1)
 
+    # Mark 4
+    mark4 = vcd.get_mark(4)
+    t = mark4.WB[0]
+    self.assertTrue(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 1)
+    self.assertEqual(self.mm_entry(vcd, t), mark4.addr-8)
+    self.assertEqual(self.mm_exit(vcd, t), mark4.addr+16)
+    t = vcd.WB2[mark4.addr+8][0]
+    self.assertTrue(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 1)
+    self.assertEqual(self.mm_entry(vcd, t), mark4.addr-8)
+    self.assertEqual(self.mm_exit(vcd, t), mark4.addr+16)
+    t = vcd.WB2[mark4.addr+12][0]
+    self.assertTrue(self.in_mm(vcd, t))
+    self.assertEqual(self.mm_depth(vcd, t), 1)
+    self.assertEqual(self.mm_entry(vcd, t), mark4.addr-8)
+    self.assertEqual(self.mm_exit(vcd, t), mark4.addr+16)
+    t = vcd.WB2[mark4.addr+16][0]
+    tn = vcd.nextt(t)
+    self.assertFalse(self.in_mm(vcd, tn))
+    self.assertEqual(self.mm_depth(vcd, tn), 0)
+    self.assertEqual(self.mm_entry(vcd, tn), 0x7FFFFFFF)
+    self.assertEqual(self.mm_exit(vcd, tn), 0x7FFFFFFF)
+
 if __name__ == '__main__':
   ite(len(sys.argv) > 1)
